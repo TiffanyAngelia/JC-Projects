3ed49cd6a637f946bef1a7f61c6c52dc1333c4c5
TiffanyAngelia <tiffanyy.angelia@gmail.com>
2025-02-20 11:36:48 +0700

    20/02/2025

--- JC2/BinaryTrees.py
+++ JC2/BinaryTrees.py
@@ -1,12 +1,42 @@
-Tree = [[-1, None, -1] for _ in range(0,10)]
+# Tree = [[-1, None, -1] for _ in range(0,10)]
 
-for i in range(len(Tree)-1):
-    Tree[i][1] = i + 1
-Tree[9][1] = -1
-print(Tree)
+# for i in range(len(Tree)-1):
+#     Tree[i][1] = i + 1
+# Tree[9][1] = -1
+# print(Tree)
 
-freePointer = 0
-rootPointer = -1
+# freePointer = 0
+# rootPointer = -1
+# oldPointer = -1
+
+Tree = [
+[1,9,2],
+[3,7,-1],
+[4,13,5],
+[-1,5,-1],
+[-1,12,-1],
+[-1,15,-1],
+[-1,7,-1],
+[-1,8,-1],
+[-1,9,-1],
+[-1,-1,-1]
+]
+
+freePointer = 6
+rootPointer = 0
 oldPointer = -1
 
-def addNode
+def searchTree(searchElement):
+    itemPointer = rootPointer
+    while itemPointer != -1 and Tree[itemPointer][1] != searchElement:
+        if searchElement > Tree[itemPointer][1]:
+            itemPointer = Tree[itemPointer][2]
+        elif searchElement < Tree[itemPointer][1]:
+            itemPointer = Tree[itemPointer][0]
+    return itemPointer
+
+print("Item is found at index: ", searchTree(12))
+print("Item is found at index: ", searchTree(9))
+print("Item is found at index: ", searchTree(13))
+print("Item is found at index: ", searchTree(999))
+
