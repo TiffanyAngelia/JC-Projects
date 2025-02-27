1babf85f8699bfdb01c4e38c05fcb18831f45f55
TiffanyAngelia <tiffanyy.angelia@gmail.com>
2025-02-27 13:44:34 +0700

    27/02/2025

--- JC2/Recursion.py
+++ JC2/Recursion.py
@@ -3,6 +3,7 @@
 # 2. Base case (Terminating condition)
 # MAKE SURE THAT THE PASSING IS CHANGING 
 
+#------------------------------------------------------  FACTORIAL
 
 def factorial(num): 
     if num == 1:
@@ -13,8 +14,9 @@ def factorial(num):
 # value = factorial(998)
 # print(value)
 
-myArr = [1,2,3,4,5,6,7,8,9,10]
+#------------------------------------------------------ SUM OF ARRAY
 
+myArr = [1,2,3,4,5,6,7,8,9,10]
 
 def SumofArray(myArr):
     if len(myArr) == 0:
@@ -22,5 +24,49 @@ def SumofArray(myArr):
     else:
         return myArr[0] + SumofArray(myArr[1:])
 
-print(SumofArray(myArr))
+# print(SumofArray(myArr))
+
+#------------------------------------------------------ REVERSE A STRING
+
+def ReverseString(String):
+    if len(String) == 0:
+        return ""
+    else:
+        return String[-1] + ReverseString(String[:-1])
+        
+#str = input("Enter a string: ")
+# print(ReverseString(str))
+
+#------------------------------------------------------ COUNTING NO. LETTERS 
+
+
+def CountLetters(Letter, String):
+    if len(String) == 0:
+        return 0
+    else:
+        if String[0] == Letter:
+            return 1 + CountLetters(Letter, String[1:])
+
+
+# str = input("Enter a string: ")
+# print(CountLetters("a", str))
+
+#------------------------------------------------------ FIBONACCI
+
+def Fibonacci(Count):
+    if Count <= 1:
+        return Count
+    else:
+        return Fibonacci(Count-1) + Fibonacci(Count-2)
+
+# for i in range(8):
+#     print(Fibonacci(i))
+
+#------------------------------------------------------ BINARY SEARCH
+
+myArr = [1,2,3,4,5,6,7,8,9]
+
+def BinarySearch(num):
+    if myArr[] == num:
+        return
 
