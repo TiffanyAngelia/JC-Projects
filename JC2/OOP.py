63d34cfa8622b93a5b39cc4f660511388cb2304e
TiffanyAngelia <tiffanyy.angelia@gmail.com>
2025-03-05 10:46:21 +0700

    05/03/2025

--- JC2/OOP.py
+++ JC2/OOP.py
@@ -3,9 +3,16 @@
 #Creating an object ---- CONSTRUCTION
 #Constructor is a special method used to contruct objects. Allows you to create personalised objects while creation. 
 
+#Encapsulation: to group together the data and all the actions of the data. Hiding the data.
+
+#Instance = Object
+#Instance variable = The variables of the object, may differ for each object (self.)
+#Non-instance variables / Class variables = The variables that belong to all objcets in the class (outside __init__)
+
 #----------------------------------------------------
 
 class Car:  #Record Data Structure TYPE DECLARATION
+
     make = ""
     model = ""
     color = ""
@@ -31,22 +38,75 @@ car1.color = "Blue"
 #----------------------------------------------------
 
 class Student:  #CLASS DECLARATION, useuppercase for first letter
+
     def __init__(self, name, subject, gender):
         self.name = name #this is parameter
         self.subject = subject
         self.gender = gender
+
     def introduction(self):  #when calling the method, PASS self to avoid error
         print(f"hello! ^3^ I am {self.name}, {self.gender} and my favourite subject is {self.subject}!!!")
 
 
 Emily = Student("Emily","Physics","Female") 
-Emily.introduction()
+# Emily.introduction()
 
 Nadine = Student("Nadine","Biology","Female") 
-Nadine.introduction()
+# Nadine.introduction()
+
+#----------------------------------------------------
+
+class Person:
+
+    personCount = 0 #Class Variable / non-instance variable
+
+    def __init__(self,name,DOB,gender): #Constructor
+        self.__name = name #of type string       #self.name is an instance variable    #self.__name is a private variable, cannot be accessed
+        self.__DOB = DOB #of type date
+        self.__gender = gender #of type string
+        Person.personCount += 1
+
+    def walk(self):
+        print(f"{self.__name} is now walking!! :D")
+
+    def Run(self):
+        print(f"{self.__name} is now running!! D:")
+    
+    #getters
+    def getName(self):
+        return self.__name
+    def getDOB(self):
+        return self.__DOB
+    def getGender(self):
+        return self.__gender        
+    #setters
+    def setName(self, name):
+        self.__name = name
+    def setDOB(self,DOB):
+        self.__DOB = DOB
+    def setGender(self,gender):
+        self.__gender = gender       
 
 
 
-#Encapsulation: to group together the data and all the actions of the data. 
+Emily = Person("Emily","25/11/2007", "Female") #Instantiating
+Nadine = Person("Nadine","07/08/2008", "Male")
+Gichelle = Person("Gichelle","14/07/2008", "Female")
 
+Emily.Run()           
+Emily.walk()
+# print(Nadine.__name) #doesn't work if private
+# print(Nadine.__DOB)
+# print(Nadine.__gender)
+# print(Nadine.__dict__)       #dictionary format
+# print(Person.personCount)
+
+print(Nadine.getName())
+print(Nadine.getDOB())
+print(Nadine.getGender())
+
+print(Nadine.__dict__)       #dictionary format
+print(Person.personCount)
+
+Emily.setName("")
 
